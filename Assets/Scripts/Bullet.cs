97ddc5211ca9469101cc2618fdc68c71539736a4
Benja-Boi <75277557+Benja-Boi@users.noreply.github.com>
2020-12-28 01:00:59 +0200

    6th Ophir Update
    -Finished player animations
    -Main Menu
    -Game Over Menu
    -Drill Demos

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject hitEffect;
+   
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Quaternion collisionDir = transform.rotation;
+        GameObject effect = Instantiate(hitEffect, transform.position, collisionDir);
+        Destroy(effect, 0.5f);
+        if (collision.gameObject.tag.Equals("Enemy"))
+        {
+            Destroy(collision.gameObject);
+        }
+        ///
+        Destroy(gameObject);
+    }
+
+
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+    public int maxHealth = 100;
+    public int currentHealth;
+    public float stdDmg = 10f;
+
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void Update()
+    {
+  
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        TakeDamage(stdDmg);
+    }
+
+    private void TakeDamage(float stdDmg)
+    {
+        currentHealth -= (int)stdDmg;
+        healthBar.SetHealth(currentHealth);
+    }
+
+}
