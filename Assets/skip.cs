cc068ec9dd6f12f3c1d4ff2e8a1bce2bbc058ff0
raz <raz.orenstein@post.idc.ac.il>
2021-01-02 18:28:00 +0200

    Raz \ Golan update
    skipable intro

---
+++ Assets/skip.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class skip : MonoBehaviour
+{
+    public void MouseClick(string buttonType)
+    {
+        if (buttonType == "Skip")
+        {
+            SceneManager.LoadScene("Gameplay");
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
